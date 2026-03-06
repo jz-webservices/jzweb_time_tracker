de760ab7ae0463c6f76ea3f7574323d6b8bb2050
jz-webservices <info@jzwebservices.at>
2026-03-06 12:53:42 +0100

    bump version to 1.2.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Time Tracker',
-    'version': '1.1.0',
+    'version': '1.2.0',
     'category': 'Productivity',
     'summary': 'Simple start/stop time tracking with calendar view',
     'description': """
