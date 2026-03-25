2bf27ea7f6142c9a161d8a6e6050225860b118b8
jz-webservices <info@jzwebservices.at>
2026-03-25 12:09:22 +0100

    Set default time rounding to 0 minutes via post_init/update hook
    Ensures rounding is always reset to 0 on install and upgrade,
    regardless of previously stored ir.config_parameter value.

--- __init__.py
+++ __init__.py
@@ -8,9 +8,17 @@ def _assign_group_to_internal_users(env):
         group.write({'users': [(4, user.id) for user in base_group.users]})
 
 
+def _reset_rounding_to_zero(env):
+    env['ir.config_parameter'].sudo().set_param(
+        'jzweb_time_tracker.rounding_minutes', '0'
+    )
+
+
 def post_init_hook(env):
     _assign_group_to_internal_users(env)
+    _reset_rounding_to_zero(env)
 
 
 def post_update_hook(env):
     _assign_group_to_internal_users(env)
+    _reset_rounding_to_zero(env)
