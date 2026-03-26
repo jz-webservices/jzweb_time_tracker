28463505a6731ff7152e2a229adf37749fd70cb2
jz-webservices <info@jzwebservices.at>
2026-03-26 17:12:41 +0100

    Update module icon to new JZWeb branded version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Time Tracker',
-    'version': '19.0.1.21.0',
+    'version': '19.0.1.22.0',
     'category': 'Productivity',
     'summary': 'Simple start/stop time tracking with calendar view',
     'description': """
